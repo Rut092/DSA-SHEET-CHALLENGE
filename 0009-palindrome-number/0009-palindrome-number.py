a2d4ff3ffecebb2f863938de83f0ab340fea6df9
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2025-12-10 13:19:25 +0530

    Time: 15 ms (18.67%), Space: 17.8 MB (63.09%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,8 +1,12 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x<0:
+        if x<0 or (x%10==0 and x!=0) :
             return False
-        num,rev= x,0
-        while(num):
-            rev,num=rev*10 + num%10,num//10
-        return rev==x
+        rev_int = 0
+
+        while(x>rev_int):
+            rev_int,x = x%10 + rev_int*10,x//10
+
+        return (x==rev_int) or (x==rev_int//10)
+
+        
