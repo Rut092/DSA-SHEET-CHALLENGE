2741adea639c1cdf0355c710c45fb20cdb62199b
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2025-12-15 15:02:43 +0530

    Time: 359 ms (11.4%), Space: 28.4 MB (93.74%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -1,10 +1,8 @@
 class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         
-        max_freq = 0
+        max_freq,i,j = 0,0,0
         nums = sorted(nums)
-        i=0
-        j=0
         sum = nums[i]
         while(j<len(nums) and i<=j):
             if nums[j]*(j-i+1)<=sum+k:
