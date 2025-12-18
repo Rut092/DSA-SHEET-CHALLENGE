97c1efbef5c64e7afb95f75f63ca7735c67cfc85
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2025-12-18 19:26:19 +0530

    Time: 0 ms (100%), Space: 12.5 MB (9.87%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -6,18 +6,13 @@ class Solution(object):
         """
         arr = []
         for i in range(len(nums)-1):
-            print(i,'i')
             if nums[i]>nums[i+1]:
-                last =-1
-                for j in range(i+1,len(nums)):
-                    print(j,'j')
-                    if j<len(nums)-1 and nums[j]>nums[j+1]:
+                for j in range(i+1,len(nums)-1):
+                    if nums[j]>nums[j+1]:
                         return False
-                    else:
-                        last = nums[j]
                 
-                if last>nums[0]:
+                if nums[-1]>nums[0]:
                     return False
-                return True
+                break
 
         return True
