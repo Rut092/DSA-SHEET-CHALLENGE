fc119b0bfbe1500fb38af5fc60a320b887533ae2
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2025-12-19 13:40:47 +0530

    Time: 0 ms (100%), Space: 18.4 MB (99.98%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,10 +4,8 @@ class Solution:
         i=0
         j=1
         while(j<len(nums)):
-            if nums[i]==nums[j]:
-                j+=1
-            else:
+            if nums[i]!=nums[j]:
                 nums[i+1]=nums[j]
                 i+=1
-                j+=1
+            j+=1
         return i+1
