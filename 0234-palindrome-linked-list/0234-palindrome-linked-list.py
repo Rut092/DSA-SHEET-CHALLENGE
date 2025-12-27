985516dd5b2c5798c5476f7e109eeaabe9bf61c9
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2025-12-27 20:40:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -9,12 +9,32 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: bool
         """
-        stack = []
+        slow = fast = head
+
+        while(fast and fast.next):
+            slow = slow.next
+            fast=fast.next.next
+        
         pointer = head
-        while(pointer!=None):
-            stack.append(pointer.val)
-            pointer = pointer.next
-        pointer = head
-        while(len(stack) and pointer.val==stack.pop()):
-            pointer=pointer.next
-        return True if len(stack)==0 else False
+        if slow==head:
+            return True
+        slow = slow.next
+        if slow==None:
+            return False
+        while(slow):
+            if slow.val==pointer.val:
+                slow=slow.next
+                pointer = pointer.next
+            else:
+                return False
+        return True
+
+        # stack = []
+        # pointer = head
+        # while(pointer!=None):
+        #     stack.append(pointer.val)
+        #     pointer = pointer.next
+        # pointer = head
+        # while(len(stack) and pointer.val==stack.pop()):
+        #     pointer=pointer.next
+        # return True if len(stack)==0 else False
