f8507b830715508dc0a3678b9fe7773f176a929f
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-01-01 22:35:01 +0530

    Time: 0 ms (100%), Space: 12.6 MB (47.76%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -6,7 +6,19 @@ class Solution(object):
         :rtype: int
         """
 
-        for i in arr:
-            if k>i:
-                k+=1
-        return k
+        # for i in arr:
+        #     if k<i:
+        #         break
+        #     k+=1
+        # return k
+
+        low,high=0,len(arr)-1
+        while(low<=high):
+            mid = (low+high)//2
+            if arr[mid]-(mid+1)<k:
+                low=mid+1
+            else:
+                high=mid-1
+        return low+k
+
+        
