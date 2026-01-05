c21684ef87b06a7dfe5f9ec3ff19159b9c3bd09c
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-01-05 16:29:12 +0530

    Time: 15 ms (5.97%), Space: 17.2 MB (93.62%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -6,10 +6,11 @@ class Solution:
         map2 = {}
 
         for i in range(len(s)):
-            if s[i] not in map1 and t[i] not in map2 :
-                map1[s[i]]=t[i]
-                map2[t[i]]=s[i]
-            elif (s[i] not in map1 and t[i] in map2) or (s[i] not in map2 and t[i] in map1) or map1[s[i]]!=t[i]:
+            if (s[i] in map1) and map1[s[i]]!=t[i]:
                 return False
+            if (t[i] in map2) and map2[t[i]]!=s[i]:
+                return False
+            map1[s[i]] = t[i]
+            map2[t[i]] = s[i]
         return True 
             
