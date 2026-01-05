9479b2958053158e083242de98a452994c419c72
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-01-05 13:54:20 +0530

    Time: 3 ms (68.41%), Space: 17.4 MB (81.88%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.py
@@ -3,15 +3,13 @@ class Solution:
         arr = []
         opened = 0
         for i in s:
-            if opened==0 and i=='(':
+            if i=='(':
+                if opened>0:
+                    arr.append(i)
                 opened+=1
-            elif i=='(':
-                opened+=1
-                arr.append(i)
-            elif i==')' and opened==1:
-                opened-=1
             else:
-                arr.append(i)
+                if opened>1:    
+                    arr.append(i)
                 opened-=1
         
         return "".join(arr)
