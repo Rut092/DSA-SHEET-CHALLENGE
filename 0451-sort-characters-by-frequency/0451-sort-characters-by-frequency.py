de5657c4423a39f195429305c1d325d4d1851379
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-01-08 16:10:15 +0530

    Time: 15 ms (30.72%), Space: 21.3 MB (18.65%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -0,0 +1,20 @@
+import heapq
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        freq_hash = {}
+        for char in s:
+            freq_hash[char] = freq_hash.get(char,0)+1
+
+        ele_freq = []
+        for key in freq_hash:
+            ele_freq.append([-freq_hash[key],key])
+
+        heapq.heapify(ele_freq)
+        ans = []
+
+        for i in range(len(freq_hash)):
+            count,ele = heapq.heappop(ele_freq)
+            for freq in range(-count):
+                ans.append(ele)
+                
+        return "".join(ans)
