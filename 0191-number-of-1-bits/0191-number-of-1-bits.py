e75678b04a79b1067be6095f84a2667a70333e22
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-01-12 18:42:04 +0530

    Time: 3 ms (19.56%), Space: 12.5 MB (13.15%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -6,7 +6,6 @@ class Solution(object):
         """
         count=0
         while(n):
-            if n&1:
-                count+=1
-            n=n>>1
+            n&=n-1
+            count+=1
         return count
