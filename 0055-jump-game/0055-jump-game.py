af2d90dc25c1e07494dc965f470bda1a99f0e083
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-01-15 22:45:18 +0530

    Time: 89 ms (8.32%), Space: 13.8 MB (5.09%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -8,10 +8,12 @@ class Solution(object):
         idx = 0
         reached = [0]*l
         reached[0]=1
+        reach = 0
         while(idx<l and reached[idx]):
-            for i in range(nums[idx]+1):
-                if idx+i<l-1:
-                    reached[idx+i]=1
+            for i in range(reach,nums[idx]+1+idx):
+                if i<l-1:
+                    reached[i]=1
+                    reach = i-1
                 else:
                     return True
             idx+=1
