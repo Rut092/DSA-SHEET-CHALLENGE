c718f0a4c8f244a68fa9801f9800eb1dff1b5392
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-01-21 15:01:25 +0530

    Time: 55 ms (13.1%), Space: 20.8 MB (20.94%) - LeetHub

---
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.py
@@ -0,0 +1,27 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+
+        max_depth = 0
+        stack = [[root,1]]
+        while(stack):
+            node,depth = stack.pop()
+            max_depth = max(max_depth,depth)
+
+            for child in node.children:
+                if child:
+                    stack.append([child,depth+1])
+
+        return max_depth
+
+            
+
