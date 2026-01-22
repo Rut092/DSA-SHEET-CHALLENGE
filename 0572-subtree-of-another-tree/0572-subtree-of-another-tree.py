e7a0eb1eb0fb00bb69af9faae26fd56b975468dc
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-01-22 10:51:34 +0530

    Time: 47 ms (33.1%), Space: 19.6 MB (23.68%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -6,43 +6,51 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        
-        starting_points = []
+        def isSame(root, subroot):
+            stack = [[root, subroot]]
+            while stack:
+                node, subnode = stack.pop()
 
-        def isPossibleSubroot(root,subRoot):
+                if not node and not subnode:
+                    continue
+                if node and subnode and node.val == subnode.val:
+                    stack.append([node.right, subnode.right])
+                    stack.append([node.left, subnode.left])
+
+                elif not node or not subnode or node.val != subnode.val:
+                    return False
+
+            return True
+
+        def isPossibleSubroot(root, subRoot):
             stack = [root]
-            while(stack):
+            while stack:
                 node = stack.pop()
 
-                if node.val==subRoot.val:
-                    starting_points.append(node)
+                if node.val == subRoot.val and isSame(node,subRoot):
+                    return True
+
                 if node.right:
                     stack.append(node.right)
                 if node.left:
                     stack.append(node.left)
 
-        def isSame(root,subroot):
-            stack=[[root,subroot]]
-            while(stack):
-                node,subnode = stack.pop()
+        def isSame(root, subroot):
+            stack = [[root, subroot]]
+            while stack:
+                node, subnode = stack.pop()
 
                 if not node and not subnode:
                     continue
-                if node and subnode and node.val==subnode.val:
-                    stack.append([node.right,subnode.right])
-                    stack.append([node.left,subnode.left])
+                if node and subnode and node.val == subnode.val:
+                    stack.append([node.right, subnode.right])
+                    stack.append([node.left, subnode.left])
 
-                elif not node or not subnode or node.val!=subnode.val:
+                elif not node or not subnode or node.val != subnode.val:
                     return False
 
             return True
 
-        isPossibleSubroot(root,subRoot)
-
-        for point in starting_points:
-            if isSame(point,subRoot):
-                return True
+        if isPossibleSubroot(root, subRoot):
+            return True
         return False
-
-                
-
