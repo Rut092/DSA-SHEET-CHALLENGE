e5aa1f2268c14d72c1181838b69b9f6bfca9b46f
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-01-22 12:03:29 +0530

    Time: 59 ms (11.09%), Space: 20.9 MB (30.88%) - LeetHub

---
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.py
@@ -0,0 +1,22 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        res = []
+        stack = [root]
+        while(stack):
+            node = stack.pop()
+            res.append(node.val)
+
+            for subnode in range(len(node.children)-1,-1,-1):
+                stack.append(node.children[subnode])
+
+        return res
