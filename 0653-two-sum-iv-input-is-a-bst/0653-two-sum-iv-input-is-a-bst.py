bc4e3852306c6ab9868640db1c1b6219e65c4a7c
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-01-22 13:56:23 +0530

    Time: 0 ms (100%), Space: 21.9 MB (22.74%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -8,13 +8,13 @@ class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
         if not root:
             return False
-        book = {}
+        book = set()
         stack = [root]
         while(stack):
             node = stack.pop()
             if k-node.val in book:
                 return True
-            book[node.val]=0
+            book.add(node.val)
             if node.right:
                 stack.append(node.right)
             if node.left:
