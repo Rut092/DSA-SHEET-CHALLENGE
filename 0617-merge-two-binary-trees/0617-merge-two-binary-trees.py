9a9c65338859d18b19748676db23f1dccdef3598
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-01-22 12:59:24 +0530

    Time: 5 ms (22.27%), Space: 19.7 MB (11.55%) - LeetHub

--- 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.py
@@ -6,26 +6,23 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root1 : return root2
+        if not root2 : return root1
 
-        stack = [[root1,root2,None,-1]]
+        stack = [[root1,root2]]
 
         while(stack):
-            node1,node2,prev,left = stack.pop()
+            node1,node2 = stack.pop()
+            node1.val+=node2.val
 
-            if not node1 and not node2:
-                continue
-
-            if node1 and node2:
-                node1.val+=node2.val
-                stack.append([node1.right,node2.right,node1,0])
-                stack.append([node1.left,node2.left,node1,1])
-
-            elif node2 and prev:
-                if not left:
-                    prev.right=node2
-                else:
-                    prev.left=node2
-            elif node2:
-                return node2
+            if node1.right and node2.right:
+                stack.append([node1.right,node2.right])
+            elif node2.right:
+                node1.right=node2.right
+            
+            if node1.left and node2.left:
+                stack.append([node1.left,node2.left])
+            elif node2.left:
+                node1.left = node2.left
         
         return root1
