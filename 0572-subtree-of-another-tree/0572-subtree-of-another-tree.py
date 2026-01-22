26f831df7f340e4cfb14b17ae1947dad023c0737
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-01-22 10:44:57 +0530

    Time: 45 ms (38.07%), Space: 19.6 MB (17.68%) - LeetHub

---
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -0,0 +1,49 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        
+        starting_points = []
+
+        def isPossibleSubroot(root,subRoot):
+            stack = [root]
+            while(stack):
+                node = stack.pop()
+
+                if node.val==subRoot.val:
+                    starting_points.append(node)
+
+                if node.right:
+                    stack.append(node.right)
+                if node.left:
+                    stack.append(node.left)
+
+        def isSame(root,subroot):
+            stack=[[root,subroot]]
+            while(stack):
+                node,subnode = stack.pop()
+
+                if not node and not subnode:
+                    continue
+                if node and subnode and node.val==subnode.val:
+                    stack.append([node.right,subnode.right])
+                    stack.append([node.left,subnode.left])
+
+                elif not node or not subnode or node.val!=subnode.val:
+                    return False
+
+            return True
+
+        isPossibleSubroot(root,subRoot)
+
+        for point in starting_points:
+            if isSame(point,subRoot):
+                return True
+        return False
+
+                
+
