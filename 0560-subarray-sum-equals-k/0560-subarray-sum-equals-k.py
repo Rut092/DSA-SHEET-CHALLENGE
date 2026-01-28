d3bce0da1e78b3a1702165ba6632f01b74ffcb7f
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-01-28 13:55:45 +0530

    Time: 42 ms (21.94%), Space: 21.5 MB (49.64%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,18 +1,17 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         count=0
-        total = [nums[0]]
         length = len(nums)
         recall = {}
         for num in range(1,length):
-            total.append(total[-1]+nums[num])
+            nums[num]+=nums[num-1]
 
-        for num in total:
+        for num in nums:
             if num==k:
                 count+=1
             if num-k in recall:
                 count+=recall[num-k]
-                
+
             if num not in recall:
                 recall[num]=1
             else:
