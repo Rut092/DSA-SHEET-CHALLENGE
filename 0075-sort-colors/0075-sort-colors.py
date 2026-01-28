5f863116f403e8f72eec9a4c1f413980b5f45217
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-01-28 15:39:14 +0530

    Time: 0 ms (100%), Space: 19.4 MB (9.98%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -3,14 +3,27 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        length = len(nums)
-        i=0
-        for j in range(length):
+        i,j,k =0,0,len(nums)-1
+        while(j<=k):
             if nums[j]==0:
-                nums[i],nums[j]=nums[j],nums[i]
+                nums[j],nums[i]=nums[i],nums[j]
                 i+=1
-        for j in range(i,length):
-            if nums[j]==1:
-                nums[i],nums[j]=nums[j],nums[i]
-                i+=1
+            elif nums[j]==2:
+                if nums[k]!=2:
+                    nums[j],nums[k]=nums[k],nums[j]
+                k-=1
+                j-=1
+
+            j+=1
+
+        # length = len(nums)
+        # i=0
+        # for j in range(length):
+        #     if nums[j]==0:
+        #         nums[i],nums[j]=nums[j],nums[i]
+        #         i+=1
+        # for j in range(i,length):
+        #     if nums[j]==1:
+        #         nums[i],nums[j]=nums[j],nums[i]
+        #         i+=1
                 
