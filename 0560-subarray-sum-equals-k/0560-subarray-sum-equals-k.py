fba3c4a8f2eb660e5962494ead4b1739a86ec217
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-02-04 14:49:39 +0530

    Time: 35 ms (48.39%), Space: 22 MB (22.49%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -3,13 +3,11 @@ class Solution:
 
         recall = {0:1}
         total = count = 0
-
         for num in nums:
             total+=num
             if total-k in recall:
                 count+=recall[total-k]
-            recall[total]=recall.get(total,0)+1
-        
+            recall[total]= recall.get(total,0)+1
         return count
 
 
