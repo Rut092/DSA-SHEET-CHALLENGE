a8197d3224f0eae66a586c1aa5955675d79105d6
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-02-09 13:40:25 +0530

    Time: 11 ms (40.12%), Space: 19.5 MB (57.91%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
@@ -2,16 +2,23 @@ class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
     
         row,col=len(grid),len(grid[0])
-        total = (row*col*(row*col+1))//2
-        res = 0
-        book={}
-        found = None
+        total = 0
         for i in range(row):
             for j in range(col):
-                res+=grid[i][j]
-                if grid[i][j] in book:
-                    found = grid[i][j]
-                else:
-                    book[grid[i][j]]=1
+                num = abs(grid[i][j])
+                total+=num
+                a = ((num-1)//row)
+                b = ((num-1)%col)
+                grid[a][b]=-grid[a][b]
 
-        return [found,abs(total-res+found)]
+        ans = []
+        for i in range(row):
+            for j in range(col):
+                if grid[i][j]>0:
+                    ans.append(i*(row)+j+1)
+        n = row*col
+        tot_sum = n*(n+1)//2
+        if tot_sum>total:
+            return ans
+        else:
+            return ans[::-1]
