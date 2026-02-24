c6df25dbc2f9fab7f4cec28fa5e76ad5379f631e
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-02-24 11:15:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -3,19 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        end = m+n-1
-        i,j=m-1,n-1
-        while(j>=0 and i>=0):
-            if nums2[j]>nums1[i]:
-                nums1[end]=nums2[j]
-                j-=1
+        total = m+n-1
+        start,end = m-1,n-1
+        while(start<total):
+            if nums1[start]>nums2[end]:
+                nums1[total]=nums1[start]
+                start-=1
             else:
-                nums1[end]=nums1[i]
-                i-=1
-            end-=1
-        while(j>=0):
-            nums1[end]=nums2[j]
-            end-=1
-            j-=1
-        
+                nums1[total]=nums2[end]
+                end-=1
+            total-=1
         
