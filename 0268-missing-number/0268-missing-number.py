740c48b579ff3cdfa589719beaf3878749fa08f6
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-02-24 11:37:14 +0530

    Time: 8 ms (28.7%), Space: 20.5 MB (61.76%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,12 +1,9 @@
-class Solution(object):
-    def missingNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        
-        xor = len(nums)
-        for i in range(xor):
-            xor^=nums[i]
-            xor^=i
-        return xor
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        truth_xor = 0
+        res_xor = nums[0]
+        l = len(nums)
+        for i in range(1,l):
+            truth_xor^=i
+            res_xor^=nums[i]
+        return truth_xor^res_xor^l
