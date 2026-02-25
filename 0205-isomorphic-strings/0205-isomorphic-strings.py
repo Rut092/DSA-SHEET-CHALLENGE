27b8fa14f91c47fe14a9aa00d5c93ee6fd4c3ce2
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-02-25 11:50:16 +0530

    Time: 3 ms (92.08%), Space: 19.3 MB (41.26%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,16 +1,19 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s)!=len(t):
-            return False
-        map1 = {}
-        map2 = {}
+        if len(s)!=len(t): return s
+        start_map = {}
+        reverse_map = {}
 
-        for i in range(len(s)):
-            if (s[i] in map1) and map1[s[i]]!=t[i]:
+        for char_s,char_t in zip(s,t):
+            if char_s not in start_map and char_t not in reverse_map:
+                start_map[char_s]=char_t
+                reverse_map[char_t]=char_s
+            elif char_s in start_map and char_t in reverse_map:
+                if start_map[char_s]!=char_t or reverse_map[char_t]!=char_s:
+                    return False
+            else:
                 return False
-            if (t[i] in map2) and map2[t[i]]!=s[i]:
-                return False
-            map1[s[i]] = t[i]
-            map2[t[i]] = s[i]
-        return True 
+            # print(start_map,reverse_map)
+        return True
+
             
