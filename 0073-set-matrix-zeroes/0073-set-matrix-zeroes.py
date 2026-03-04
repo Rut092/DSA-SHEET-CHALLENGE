a66030294437b7d7a56536d9bb3e633709c9ff8a
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-03-04 15:42:10 +0530

    Time: 7 ms (19.12%), Space: 20 MB (85.98%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -5,22 +5,24 @@ class Solution:
         """
         rows,cols = len(matrix),len(matrix[0])
         is_first_col = False
-        
+
         for row in range(rows):
             if matrix[row][0]==0:
                 is_first_col = True
             for col in range(1,cols):
                 if matrix[row][col]==0:
-                    matrix[row][0]=0
                     matrix[0][col]=0
-                
+                    matrix[row][0]=0
+
         for row in range(rows-1,-1,-1):
             for col in range(cols-1,0,-1):
-                if matrix[row][0]==0 or matrix[0][col]==0:
+                if matrix[0][col]==0 or matrix[row][0]==0:
                     matrix[row][col]=0
+
             if is_first_col:
                 matrix[row][0]=0
 
+        return matrix
 
 
 
