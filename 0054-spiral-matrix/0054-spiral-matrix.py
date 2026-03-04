0a3fb55a3e2da3386a89cc3827aa41e696b0fa2a
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-03-04 16:16:05 +0530

    Time: 0 ms (100%), Space: 19.3 MB (87.41%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,31 +1,28 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        rows,cols = len(matrix),len(matrix[0])
-        res = []
-        i,j=0,0
-        res.append(matrix[0][0])
-        matrix[0][0]=-2**8
-        while(True):
-            while(j+1<cols and matrix[i][j+1]!=-2**8):
-                res.append(matrix[i][j+1])
-                matrix[i][j+1]=-2**8
-                j+=1
-
-            while(i+1<rows and matrix[i+1][j]!=-2**8):
-                res.append(matrix[i+1][j])
-                matrix[i+1][j]=-2**8
-                i+=1
+        if not matrix: return []
     
-            while(j-1>=0 and matrix[i][j-1]!=-2**8):
-                res.append(matrix[i][j-1])
-                matrix[i][j-1]=-2**8
-                j-=1
-                
-            while(i-1>=0 and matrix[i-1][j]!=-2**8):
-                res.append(matrix[i-1][j])
-                matrix[i-1][j]=-2**8
-                i-=1
-            if len(res)==rows*cols:
-                break
+        res = []
+        top,bottom=0,len(matrix)-1
+        left,right=0,len(matrix[0])-1
+        while(top<=bottom and left<=right):
+            #moving right
+            for j in range(left,right+1):
+                res.append(matrix[top][j])
+            top+=1
+            #move down
+            for i in range(top,bottom+1):
+                res.append(matrix[i][right])
+            right-=1
+            #move left
+            if top<=bottom:
+                for j in range(right,left-1,-1):
+                    res.append(matrix[bottom][j])
+                bottom-=1
+            # move top
+            if left<=right:
+                for i in range(bottom,top-1,-1):
+                    res.append(matrix[i][left])
+                left+=1
 
         return res
