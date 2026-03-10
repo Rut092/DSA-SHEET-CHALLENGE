4d4dfe1f99a710adaa66af40ccd9fa496d384f2b
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-03-10 14:34:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,17 +1,20 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
         l = len(nums)
-        memo = [0]*(l+1)
-        for num in nums:
-            if num>l:
-                memo[l]+=1
+        low,high = 0,l
+        while(low<=high): 
+            mid = (low+high)//2
+            count = 0
+
+            for num in nums:
+                if num>=mid:
+                    count+=1
+
+            if count==mid:
+                return mid
+            elif count>mid:
+                low = mid+1
             else:
-                memo[num]+=1
-        
-        for i in range(l-1,-1,-1):
-            memo[i]+=memo[i+1]
-        
-        for i in range(l+1):
-            if i==memo[i]:
-                return i
+                high = mid-1
+
         return -1
