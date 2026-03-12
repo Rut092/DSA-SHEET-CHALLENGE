55fc5c848e3719fd9ea991a2ed78d6adb24c5d77
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-03-12 14:05:43 +0530

    Time: 236 ms (29.44%), Space: 24 MB (56.66%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -19,7 +19,7 @@ class Solution:
         for weight in weights:
             if weight>capacity:
                 return 2**32
-            elif curr+weight<capacity:
+            elif curr+weight<=capacity:
                 curr+=weight
             else:
                 total+=1
