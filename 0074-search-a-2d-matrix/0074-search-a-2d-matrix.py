04190b2a210ad4480f4de00f6dd34918b499aa50
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-03-20 12:02:56 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        rows,cols = len(matrix),len(matrix[0])
+        low,high = 0,rows*cols
+
+        while(low<=high):
+            mid = (low+high)//2
+            row,col = (mid//cols),(mid%(cols))
+    
+            if matrix[row][col]==target:
+                return True
+            elif matrix[row][col]>target:
+                high = mid-1
+            else:
+                low = mid+1
+
+        return False
