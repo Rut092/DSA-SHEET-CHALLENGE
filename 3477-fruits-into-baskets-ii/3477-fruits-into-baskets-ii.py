a20e1174388601393997f97ba03a3e1a487841fd
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-03-24 20:54:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.py
+++ 3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.py
@@ -1,3 +1,43 @@
+class Segment:
+    def __init__(self,data):
+        self.n = len(data)
+        self.tree = [0]*(4*self.n)
+
+        if self.n>0:
+            self._build(data,1,0,self.n-1)
+    
+    def _build(self,data,node,start,end):
+
+        if start == end:
+            self.tree[node] = data[start]
+            return
+
+        mid = (start+end)//2
+        self._build(data,2*node,start,mid)
+        self._build(data,2*node+1,mid+1,end)
+
+        self.tree[node] = max(self.tree[2*node],self.tree[2*node+1])
+
+    def update(self,node,start,end,val):
+        if val>self.tree[node]:
+            return False
+
+        if start == end:
+            self.tree[node]= -1
+            return True
+
+        mid = (start+end)//2
+
+        found = self.update(node*2,start,mid,val)
+
+        if not found: 
+            found = self.update(node*2+1,mid+1,end,val)
+
+        self.tree[node] = max(self.tree[2*node],self.tree[2*node+1])
+
+        return found
+
+
 class Solution(object):
     def numOfUnplacedFruits(self, fruits, baskets):
         """
@@ -7,15 +47,11 @@ class Solution(object):
         """
         fruits_length = len(fruits)
         basket_length = len(baskets)
-
+        st = Segment(baskets)
         leftout = 0
 
         for i in range(fruits_length):
-            for j in range(0,basket_length):
-                if baskets[j]>=fruits[i]:
-                    baskets[j] = 0 
-                    leftout-=1
-                    break
-            leftout+=1      
+            if  not st.update(1,0,basket_length-1,fruits[i]):
+                leftout+=1    
         
         return leftout
