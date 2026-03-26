8345a530f1b655d34d8d9507688134ca1630dfd9
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-03-26 18:29:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.py
@@ -4,5 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        return " ".join(s.split()[::-1])
-         
+        res = []
+        temp = ''
+        l = len(s)
+        start,end = 0,l-1
+
+        for i in range(start,end+1):
+            if s[i]==" ":
+                if len(temp)>0:
+                    res.append(temp)
+                    temp =''
+            else:
+                temp+=s[i]
+        
+        if temp: res.append(temp)
+
+        start,end = 0,len(res)-1
+        while(start<end):
+            res[start],res[end]=res[end],res[start]
+            start+=1
+            end-=1
+
+        return " ".join(res)
