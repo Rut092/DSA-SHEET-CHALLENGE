002894dec42ba34c3200bad2bea1a8b7cfc677b5
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-03-29 11:05:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -7,24 +7,14 @@ class Solution(object):
         l = len(s)
         low,high=0,0
         for i in range(l):
-            mini,maxi=i,i
-            while(mini>=0 and maxi<l):
-                if s[maxi]==s[mini]:
-                    if high-low<maxi-mini:
-                        high,low = maxi,mini
-                else:
-                    break
-                maxi+=1
-                mini-=1
-
-            mini,maxi=i,i+1
-            while(mini>=0 and maxi<l):
-                if s[maxi]==s[mini]:
-                    if high-low<maxi-mini:
-                        high,low = maxi,mini
-                else:
-                    break
-                maxi+=1
-                mini-=1
+            for mini,maxi in [[i,i],[i,i+1]]:
+                while(mini>=0 and maxi<l):
+                    if s[maxi]==s[mini]:
+                        if high-low<maxi-mini:
+                            high,low = maxi,mini
+                    else:
+                        break
+                    maxi+=1
+                    mini-=1
         
         return s[low:high+1]
