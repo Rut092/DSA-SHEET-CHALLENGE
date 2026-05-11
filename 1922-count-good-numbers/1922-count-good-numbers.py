bece57cefcd9319413ce020ee0706ee07c82fb50
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-05-11 13:15:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -5,13 +5,15 @@ class Solution:
             return 5
 
         def pow(num):
-            if num==1:
-                return 20
-            elif num%2==0:
-                val = pow(num//2)
+            if num==0:
+                return 1
+
+            val = pow(num//2)
+
+            if num%2==0:
                 return (val*val)% MOD
             else:
-                return (20*pow(num-1))%MOD
+                return (20*val*val)%MOD
         
         val = pow(n//2)
         if n%2==0:
