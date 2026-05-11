dbacdf29aeaaf74e29ea27b6cf4e28478dca01fb
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-05-11 13:10:02 +0530

    Time: 0 ms (100%), Space: 19.5 MB (5.82%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.py
+++ 1922-count-good-numbers/1922-count-good-numbers.py
@@ -1,13 +1,23 @@
 class Solution:
     def countGoodNumbers(self, n: int) -> int:
-        count=1
-        num = 20
-        seq = n//2
-        while(seq):
-            if seq%2==0:
-                num= (num*num)%(10**9+7)
-                seq//=2
+        MOD = 10**9 + 7
+        if n==1:
+            return 5
+
+        def pow(num):
+            print(num)
+            if num==1:
+                return 20
+            elif num%2==0:
+                val = pow(num//2)
+                return (val*val)% MOD
             else:
-                count= (count*num)%(10**9+7)
-                seq-=1
-        return count%(10**9+7) if n%2==0 else count*5%(10**9+7)
+                return (20*pow(num-1))%MOD
+        
+        val = pow(n//2)
+        
+        if n%2==0:
+            return val
+        else:
+            return (val*5)%MOD
+        
