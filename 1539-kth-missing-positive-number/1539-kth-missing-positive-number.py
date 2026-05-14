9f3003064fc76eaf3cabac1593514539bc739f04
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-05-14 18:37:56 +0530

    Time: 0 ms (100%), Space: 12.5 MB (50.78%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -1,7 +1,17 @@
-class Solution:
-    def findKthPositive(self, arr: List[int], k: int) -> int:
-        for num in arr:
-            if num>k:
-                break
-            k+=1
-        return k
+class Solution(object):
+    def findKthPositive(self, arr, k):
+        """
+        :type arr: List[int]
+        :type k: int
+        :rtype: int
+        """
+        start,end = 0,len(arr)-1
+        while(start<=end):
+            mid = (start+end)>>1
+    
+            if arr[mid]-(mid+1)<k:
+                start = mid+1
+            else:
+                end = mid-1
+        
+        return start+k
