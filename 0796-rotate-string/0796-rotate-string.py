36672a4c371616a2e7487c6874b3199e505f8621
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-05-19 11:10:54 +0530

    Time: 0 ms (100%), Space: 12.3 MB (53.07%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,35 +1,39 @@
-class Solution:
-    def rotateString(self, s: str, goal: str) -> bool:
-        heystack = goal + goal 
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
         l = len(s)
-        l_h = len(heystack)
-        if l+l!=l_h: return False
+        if l!= len(goal):
+            return False
+        heystack = goal+goal
         lps = [0]*l
         prevLPS,i=0,1
         while(i<l):
             if s[i]==s[prevLPS]:
-                lps[i] = prevLPS + 1
-                prevLPS+=1
+                lps[i]=prevLPS+1
                 i+=1
+                prevLPS+=1
             elif prevLPS==0:
                 lps[i]=0
                 i+=1
             else:
                 prevLPS = lps[prevLPS-1]
-            
-        i,j=0,0
-        while(i<l_h):
-            if s[j]==heystack[i]:
+        
+        i = j = 0
+        while(i<len(heystack)):
+            if heystack[i]==s[j]:
                 i+=1
                 j+=1
             else:
                 if j==0:
                     i+=1
                 else:
-                    j=lps[j-1]
+                    j = lps[j-1]
+    
+            if j>=l:
+                return True
 
-            if l==j: return True
         return False
-
-
-
