151d03123285a0cc05d686e3ab2aa79119928ae9
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-06-02 10:22:51 +0530

    Time: 19 ms (50.55%), Space: 18.2 MB (49.47%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+import heapq
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[Optional[ListNode]]
+        :rtype: Optional[ListNode]
+        """
+        n = len(lists)
+        min_node = []
+        for i in range(n):
+            if (lists[i])>0:
+                min_node.append([lists[i].val,lists[i]])
+
+        heapq.heapify(min_node)
+        dummy = ListNode(-1)
+        curr = dummy
+
+        while(min_node):
+            _,node = heapq.heappop(min_node)
+            curr.next = node
+            curr = curr.next
+            if node.next:
+                heapq.heappush(min_node,[node.next.val,node.next])
+
+        curr.next = None
+        return dummy.next
