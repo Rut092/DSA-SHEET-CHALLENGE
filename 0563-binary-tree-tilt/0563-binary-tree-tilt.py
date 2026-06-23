c8325e6243ee9f96e28b0a8b1f6b76505542a47c
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-06-23 15:51:55 +0530

    Time: 7 ms (64.84%), Space: 15.9 MB (5.02%) - LeetHub

--- 0563-binary-tree-tilt/0563-binary-tree-tilt.py
+++ 0563-binary-tree-tilt/0563-binary-tree-tilt.py
@@ -1,31 +1,30 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def findTilt(self, root: Optional[TreeNode]) -> int:
+class Solution(object):
+    def findTilt(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if not root: return 0
         
-        if not root:
-            return 0
-
         self.total = 0
 
         def simplify(root):
-            left_sum = 0
-            right_sum = 0
 
-            if root.left:
-                left_sum = simplify(root.left)
-            if root.right:
-                right_sum = simplify(root.right)
+            left = 0 if not root.left else simplify(root.left)
+            right = 0 if not root.right else simplify(root.right)
 
-            original = root.val
-            root.val = abs(left_sum - right_sum)
-            self.total+=root.val
+            data = root.val 
 
-            return left_sum + right_sum + original
+            root.data = abs(left-right)
+            self.total+=root.data
+        
+            return data + left + right
 
         simplify(root)
         return self.total
