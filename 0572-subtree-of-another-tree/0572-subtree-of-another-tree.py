7435b34dec8d7ae0e314c8ad8d8546bd5b630a58
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-07-03 12:12:48 +0530

    Time: 14 ms (97.09%), Space: 12.5 MB (100%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -1,63 +1,61 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        paths = []
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: Optional[TreeNode]
+        :type subRoot: Optional[TreeNode]
+        :rtype: bool
+        """
+        needle = self.getPaths(subRoot)
+        heystack = self.getPaths(root)
 
-        def getPaths(root):
-
-            arr = []
-            stack = [root]
-            while(stack):
-                node = stack.pop()
-                if node:
-                    arr.append(str(node.val))
-                    stack.append(node.right)
-                    stack.append(node.left)
-                else:
-                    arr.append('+')
-
-            paths.append(arr)
-
-        getPaths(root)
-        getPaths(subRoot)
-
-        haystack = paths[0]
-        needle = paths[1]
-        n_len,h_len=len(needle),len(haystack)
+        n_len,h_len = len(needle),len(heystack)
         lps = [0]*n_len
-
-        prevLPS,i=0,1
+        prevLPS,i = 0,1
+        
         while(i<n_len):
             if needle[i]==needle[prevLPS]:
-                lps[i]=prevLPS + 1
+                lps[i] = prevLPS + 1
                 i+=1
                 prevLPS+=1
-
+            
             elif prevLPS==0:
                 lps[i]=0
                 i+=1
             else:
-                prevLPS = lps[prevLPS-1]
-        
-        i,j=0,0
+                prevLPS = lps[prevLPS - 1]
 
-        while(i<h_len):
-            if haystack[i]==needle[j]:
+        i,j = 0,0
+        while(j<h_len):
+            if needle[i]==heystack[j]:
                 i+=1
                 j+=1
             else:
-                if j==0:
-                    i+=1
+                if i==0:
+                    j+=1
                 else:
-                    j=lps[j-1]
-
-            if j==n_len:
+                    i = lps[i-1]
+            
+            if i==n_len:
                 return True
 
         return False
 
+    def getPaths(self,root):
+        path = []
+        stack = [root]
+        while(stack):
+            node = stack.pop()
+            if node:
+                path.append(str(node.val))
+                stack.append(node.right)
+                stack.append(node.left)
+            else:
+                path.append('#')
+        
+        return path
