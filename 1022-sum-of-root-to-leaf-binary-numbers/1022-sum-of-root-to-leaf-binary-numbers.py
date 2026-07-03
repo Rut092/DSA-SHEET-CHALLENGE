f32f5e6f73eb7af9cc6659519a89829d48b806fe
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-07-03 14:41:09 +0530

    Time: 2 ms (40.96%), Space: 12.9 MB (84.34%) - LeetHub

--- 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
+++ 1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.py
@@ -1,27 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        total = 0
-        stack=[[root,0]]
+from collections import deque
+class Solution(object):
+    def sumRootToLeaf(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        curr_sum = 0
+        q = deque([[root,root.val]])
 
-        while(stack):
-            node,value = stack.pop()
-            bit_val = value<<1 | node.val
-
-            if not node.left and not node.right:
-                total+=bit_val
-            if node.right:
-                stack.append([node.right,bit_val])
-            if node.left:
-                stack.append([node.left,bit_val])
-
-        return total
-
-            
+        while(q):
+            l = len(q)
+            for i in range(l):
+                node,val = q.popleft()
+                if node.left or node.right:
+                    if node.left:
+                        q.append([node.left,val<<1|node.left.val])
+                    if node.right:
+                        q.append([node.right,val<<1|node.right.val])
+                else:
+                    curr_sum+=val
+        return curr_sum
