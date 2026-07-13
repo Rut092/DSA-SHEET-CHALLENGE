d4c96b12b82c395a1d146935b50d79001dfd75b0
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-07-13 14:57:52 +0530

    Time: 0 ms (100%), Space: 12.5 MB (19.21%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,25 +1,28 @@
-class Solution:
-    def nextPermutation(self, nums: List[int]) -> None:
+class Solution(object):
+    def nextPermutation(self, nums):
         """
-        Do not return anything, modify nums in-place instead.
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
         """
         l = len(nums)
         idx = -1
-        for i in range(l-1,0,-1):
-            if nums[i-1]<nums[i]:
-                idx = i-1
+        for i in range(l-2,-1,-1):
+            if nums[i]<nums[i+1]:
+                idx = i
                 break
+        
         if idx!=-1:
             for i in range(l-1,idx-1,-1):
                 if nums[idx]<nums[i]:
                     nums[idx],nums[i]=nums[i],nums[idx]
                     break
-    
-        idx+=1
+
         l-=1
+        idx+=1
         while(idx<l):
             nums[idx],nums[l]=nums[l],nums[idx]
             idx+=1
             l-=1
-        
 
+        
+        
