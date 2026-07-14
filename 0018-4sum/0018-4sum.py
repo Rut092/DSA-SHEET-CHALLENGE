03a23960e139182ad42cb7fe6e872e3fa22a32fd
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-07-14 12:23:44 +0530

    Time: 426 ms (92.62%), Space: 12.4 MB (64.44%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -5,33 +5,42 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        nums.sort()
         l = len(nums)
-        res=[]
-        for a in range(l-3):
-            if nums[a]+nums[a+1]+nums[a+2]+nums[a+3]>target:
-                break
-            if a>0 and nums[a]==nums[a-1]:
-                continue
-            for b in range(a+1,l-2):
-                if b-1>a and nums[b]==nums[b-1]:
-                    continue
-                c=b+1
-                d=l-1
+        nums.sort()
+        res = []
 
-                while(c<d):
-                    total = nums[a]+nums[b]+nums[c]+nums[d]
-                    if total==target:
-                        res.append([nums[a],nums[b],nums[c],nums[d]])
-                        while(c<d and nums[c]==nums[c+1]):
-                            c+=1
-                        c+=1
-                        while(c<d and nums[d]==nums[d-1]):
-                            d-=1
-                        d-=1
-                    elif total<target:
-                        c+=1
+        for i in range(l-3):
+            if i+3<l and nums[i] + nums[i+1] + nums[i+2] + nums[i+3]>target:
+                return res
+            if i!=0 and nums[i-1]==nums[i]:
+                continue
+            for j in range(i+1,l-2):
+                if j+2<l and nums[i]+nums[j] + nums[j+1] + nums[j+2]>target:
+                    break
+                if (j-1>i and nums[j-1]==nums[j]):
+                    continue
+                
+                start,end = j+1,l-1
+                while(start<end):
+                    total = nums[i] + nums[j] + nums[start] + nums[end]
+
+                    if total == target:
+                        res.append([nums[i],nums[j],nums[start],nums[end]])
+                        
+                        while(start+1<l and nums[start]==nums[start+1]):
+                            start+=1
+                        while(end>0 and nums[end]==nums[end-1]):
+                            end-=1
+                        start+=1
+                        end-=1
+
+                    elif total > target:
+                        while(end>0 and nums[end]==nums[end-1]):
+                            end-=1
+                        end-=1
                     else:
-                        d-=1
-        
+                        while(start+1<l and nums[start]==nums[start+1]):
+                            start+=1
+                        start+=1
+
         return res
