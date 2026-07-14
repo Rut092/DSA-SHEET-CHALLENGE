139b35739c9cb0ae60ce21357f8ca6aa92ce788e
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-07-14 11:47:36 +0530

    Time: 10 ms (64.85%), Space: 14.1 MB (72.48%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -6,25 +6,34 @@ class Solution(object):
         """
         num1 = num2 = None
         count1 = count2 = 0
+
         for num in nums:
             if num==num1:
                 count1+=1
             elif num==num2:
                 count2+=1
-            elif count1 == 0:
+
+            elif count1==0:
                 num1 = num
-                count1 = 1
-            elif count2 == 0:
+                count1=1
+            elif count2==0:
                 num2 = num
                 count2=1
             else:
                 count1-=1
                 count2-=1
-
-        n_o_3 = len(nums)//3
-        res = []
-        for num in [num1,num2]:
-            if num is not None and nums.count(num)>n_o_3:
-                res.append(num)
         
+        count1 = count2 = 0
+        for num in nums:
+            if num==num1:
+                count1+=1
+            elif num==num2:
+                count2+=1
+
+        res = []
+        if count1>len(nums)/3:
+            res.append(num1)
+        if count2>len(nums)/3:
+            res.append(num2)
+
         return res
