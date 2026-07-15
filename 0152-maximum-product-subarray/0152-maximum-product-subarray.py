2b36129c65e3639b9a6043acd2261ab85e905aca
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-07-15 23:24:12 +0530

    Time: 4 ms (98.44%), Space: 13.2 MB (30.19%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,15 +1,22 @@
-class Solution:
-    def maxProduct(self, nums: List[int]) -> int:
-        prefix = suffix = 1
-        maxi=float('-inf')
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxi = float('-inf')
+        pre = suff = 1
         l = len(nums)
+
         for i in range(l):
-            prefix*=nums[i]
-            suffix*=nums[l-i-1]
-            maxi=max(maxi,prefix,suffix)
-            if prefix==0:
-                prefix=1
-            if suffix==0:
-                suffix=1
+            pre*=nums[i]
+            suff*=nums[l-i-1]
+
+            maxi = max(maxi,pre,suff)
+
+            if pre==0:
+                pre=1
+            if suff==0:
+                suff = 1
 
         return maxi
