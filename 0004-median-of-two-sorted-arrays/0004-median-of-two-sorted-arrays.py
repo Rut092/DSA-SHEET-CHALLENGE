d1ce408895b374e4f06d31ece511ff7258316054
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-07-23 15:58:03 +0530

    Time: 11 ms (9.31%), Space: 12.6 MB (13.87%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -1,18 +1,21 @@
-class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        if len(nums1)>len(nums2):
-            return self.findMedianSortedArrays(nums2,nums1)
-            
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        if len(nums1)>len(nums2): return self.findMedianSortedArrays(nums2,nums1)
         l1,l2 = len(nums1),len(nums2)
-        low,high = 0,l1
-        l = l1+l2
-        left = (l+1)//2
+        total = l1+l2
+        low,high =0,l1
+        left = (total+1)//2
 
         while(low<=high):
             mid1 = (low+high)>>1
-            mid2 = left-mid1
             a1 = a2 = float('-inf')
             x1 = x2 = float('inf')
+            mid2 = left-mid1
 
             if mid1<l1: x1 = nums1[mid1]
             if mid2<l2: x2 = nums2[mid2]
@@ -24,9 +27,8 @@ class Solution:
             elif a2>x1:
                 low = mid1+1
             else:
-                if l%2!=0:
-                    return max(a1,a2)
-                return (max(a1,a2)+min(x1,x2))/2
-
+                if total%2==0:
+                    return float((max(a1,a2) + min(x2,x1)))/2
+                return max(a1,a2)
 
         return 0
