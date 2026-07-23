54d93e703b10deacc7ac50bfc3d0ccef9326e86a
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-07-23 13:02:18 +0530

    Time: 445 ms (73.75%), Space: 21.5 MB (21.14%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.py
@@ -1,30 +1,32 @@
-class Solution:
-    def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
-        l = len(bloomDay)
-        low,high = min(bloomDay),max(bloomDay)
-        ans = -1
-        while(low<=high):
-            mid = (low+high)//2
-            count = self.total_boquets(mid,bloomDay,k)
-
-            if count<m:
+class Solution(object):
+    def minDays(self, bloomDay, m, k):
+        """
+        :type bloomDay: List[int]
+        :type m: int
+        :type k: int
+        :rtype: int
+        """
+        maxi = max(bloomDay)
+        low,high = 1,maxi+1
+        while(low<high):
+            mid = (low+high)>>1
+            getVal = self.findDays(mid,m,k,bloomDay)
+            if getVal ==True:
+                high = mid
+            else:
                 low = mid+1
+
+        return high if high<maxi+1 else -1
+    
+    def findDays(self,days,m,k,bloomDay):
+        boq = 0
+        flower = k
+        for i in bloomDay:
+            if i<=days:
+                flower-=1
+                if flower==0:
+                    boq+=1
+                    flower = k
             else:
-                ans = mid
-                high = mid-1
-
-        return ans
-
-    def total_boquets(self,value,bloomDay,flowers):
-        boquets_count = count_flower = 0
-
-        for flo in bloomDay:
-            if flo<=value:
-                count_flower+=1
-                if count_flower%flowers==0:
-                    boquets_count+=1
-                    count_flower=0
-            else:
-                count_flower=0
-
-        return boquets_count
+                flower = k
+        return True if boq>=m else False
