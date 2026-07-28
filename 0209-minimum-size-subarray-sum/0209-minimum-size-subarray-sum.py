bab840152c480fbdfa4a8ce167b298c1cc7cc69e
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-07-28 15:17:59 +0530

    Time: 374 ms (5.1%), Space: 20.8 MB (14.79%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -5,14 +5,31 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        l = len(nums)
-        curr_sum = left = 0
-        ans = float('inf')
-        for right in range(l):
-            curr_sum+=nums[right]
-            while(curr_sum>=target):
-                ans = min(ans,right-left)
-                curr_sum-=nums[left]
-                left+=1
+        # l = len(nums)
+        # curr_sum = left = 0
+        # ans = float('inf')
+        # for right in range(l):
+        #     curr_sum+=nums[right]
+        #     while(curr_sum>=target):
+        #         ans = min(ans,right-left)
+        #         curr_sum-=nums[left]
+        #         left+=1
         
-        return ans+1 if ans!=float('inf') else 0
+        # return ans+1 if ans!=float('inf') else 0
+        l = len(nums)
+        for i in range(1,l):
+            nums[i]+=nums[i-1]
+        nums = [0]+nums
+        ans = float('inf')
+        for i in range(l+1):
+            find = target + nums[i]
+            low,high = i+1,l
+            while(low<=high):
+                mid = (low+high)>>1
+                if nums[mid]>=find:
+                    ans = min(ans,mid-i)
+                    high = mid-1
+                else:
+                    low = mid+1
+            
+        return ans if ans!=float('inf') else 0
