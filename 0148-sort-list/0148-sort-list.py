6e9cb2f68c4ac1fb528bdde359e8474247ed2a2e
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-07-30 16:16:31 +0530

    Time: 449 ms (20.7%), Space: 50.6 MB (75.11%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -1,51 +1,49 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        if not head or not head.next:
-            return head
+class Solution(object):
+    def sortList(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        if not head or not head.next : return head
 
-        mid_prev = self.mid(head)
+        mid_prev = self.middle(head)
         middle = mid_prev.next
         mid_prev.next = None
 
-        left = self.sortList(head)
-        right = self.sortList(middle)
-
+        left,right = self.sortList(head),self.sortList(middle)
         return self.conquer(left,right)
 
-    def mid(self,start):
-        slow = start
-        fast = start.next
+    def conquer(self,left,right):
+        node = ListNode(0)
+        curr = node
+        while(left and right):
+            if left.val<right.val:
+                curr.next = left
+                left = left.next
+            else:
+                curr.next = right
+                right = right.next
+            curr = curr.next
+
+        if left:
+            curr.next = left
+        if right:
+            curr.next = right
+
+        return node.next
+
+    def middle(self,linkedList):
+        fast,slow = linkedList.next,linkedList
         while(fast and fast.next):
             fast = fast.next.next
             slow = slow.next
-
         return slow
+        
 
-    
-    def conquer(self,left,right):
-        Dummy = ListNode(0)
-        merged = Dummy
 
-        while(left and right):
-            if left.val>right.val:
-                merged.next = right
-                right = right.next
-            else:
-                merged.next = left
-                left = left.next
-                
-            merged = merged.next
-
-        if (left):
-            merged.next = left
-
-        if(right):
-            merged.next = right
-
-        return Dummy.next
+        
