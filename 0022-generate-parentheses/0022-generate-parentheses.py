479835c2a6e4553e776e74112c3819ae4a844fb9
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-08-10 13:15:59 +0530

    Time: 1 ms (58.9%), Space: 12.8 MB (19.54%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -6,18 +6,16 @@ class Solution(object):
         """
         res = []
 
-        def calculate(opened,closed,temp,val):
+        def calculate(opened,closed,val):
             if opened==n and closed==n:
                 res.append("".join(val))
                 return 
             
             if opened<n:
-                calculate(opened+1,closed,temp+['('],val+['('])
+                calculate(opened+1,closed,val+['('])
 
             if opened>closed:
-                if temp[-1]=='(':
-                    temp.pop()
-                    calculate(opened,closed+1,temp,val+[')'])
+                calculate(opened,closed+1,val+[')'])
             
-        calculate(0,0,[],[])
+        calculate(0,0,[])
         return res
