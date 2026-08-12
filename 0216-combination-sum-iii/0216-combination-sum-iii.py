b96af6799744e920c2d73a9c6dac8e80b3313dc1
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-08-12 11:49:31 +0530

    Time: 0 ms (100%), Space: 12.4 MB (19.79%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -9,13 +9,17 @@ class Solution(object):
 
         def calc(count,total,array,num):
             if num==total and count==k-1:
-                res.append(array[:]+[num])
+                array.append(num)
+                res.append(array[:])
+                array.pop()
                 return
             elif num>total or count>=k:
                 return
             else:
+                array.append(num)
                 for i in range(num+1,10):
-                    calc(count+1,total-num,array+[num],i)
+                    calc(count+1,total-num,array,i)
+                array.pop()
 
         for number in range(1,8):
             calc(0,n,[],number)
