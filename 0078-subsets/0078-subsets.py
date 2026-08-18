5f5f56f40c4f1f8299bd3f5c0995e3acc638f7a7
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-08-18 14:13:24 +0530

    Time: 0 ms (100%), Space: 12.6 MB (23.91%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -5,16 +5,6 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         res = []
-        # def calc(index,value):
-        #     if index==len(nums):
-        #         res.append(value)
-        #         return
-        #     calc(index+1,value)
-        #     calc(index+1,value+[nums[index]])
-        
-        # calc(0,[])
-
-        # Using Bit Manipulation
         n = len(nums)
         subsets = (1<<n)
         for num in range(subsets):
