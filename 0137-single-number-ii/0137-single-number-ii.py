2f974c2d8245512c8929e5f1d615dcb22e7a1d1d
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-08-18 10:29:43 +0530

    Time: 4 ms (50.25%), Space: 13.4 MB (70.32%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -4,14 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        thief = 0
-        for i in range(31,-1,-1):
-            count = 0
-            for num in nums:
-                if (num>>i)&1==1:
-                    count+=1
-            count%=3
-            thief = thief<<1 | count
-        
-        if thief>=(1<<31): thief-=(1<<32)
-        return thief
+        ones,twos = 0, 0
+        for num in nums:
+            ones = (ones^num)&~twos
+            twos = (twos^num)&~ones
+        return ones
