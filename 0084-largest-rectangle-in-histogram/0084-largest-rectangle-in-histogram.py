12fa7eec64c534a1f98ba21e1e9958c5a8f238be
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-08-31 16:06:00 +0530

    Time: 91 ms (99.01%), Space: 31.1 MB (62.87%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,29 +1,21 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
         l = len(heights)
-        pse,nse = self.pse(heights,l),self.nse(heights,l)
         maxi = 0
+        stack = []
         for i in range(l):
-            maxi = max(maxi,(nse[i]-pse[i]-1)*heights[i])
+            while(stack and heights[stack[-1]]>heights[i]):
+                ele_idx = stack.pop()
+                nse = i
+                pse = -1 if not stack else stack[-1]
+                maxi = max(maxi,(nse-pse-1)*heights[ele_idx])
+
+            stack.append(i)
+
+        while(stack):
+            ele_idx = stack.pop()
+            nse = l
+            pse = -1 if not stack else stack[-1]
+            maxi = max(maxi,(nse-pse-1)*heights[ele_idx])
+
         return maxi
-
-
-    def nse(self,height,l):
-        res,stack =[],[]
-        for i in range(l-1,-1,-1):
-            while(stack and height[stack[-1]]>height[i]):
-                stack.pop()
-            curr = l if not stack else stack[-1]
-            res.append(curr)
-            stack.append(i)
-        return res[::-1]
-
-    def pse(self,height,l):
-        res,stack = [],[]
-        for i in range(l):
-            while(stack and height[stack[-1]]>=height[i]):
-                stack.pop()
-            curr = -1 if not stack else stack[-1]
-            res.append(curr)
-            stack.append(i)
-        return res
