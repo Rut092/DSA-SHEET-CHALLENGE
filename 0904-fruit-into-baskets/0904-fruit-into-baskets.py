e5ece433c1431f2a27a458d112597695354fe9ed
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-09-07 20:41:09 +0530

    Time: 120 ms (97.9%), Space: 17.1 MB (59.3%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,40 +1,19 @@
+import collections
 class Solution(object):
     def totalFruit(self, fruits):
         """
         :type fruits: List[int]
         :rtype: int
         """
-        maxi = i = 0
-        b1 = b2 = None
-        b1_count = b2_count = 0
+        basket = collections.defaultdict(int)
+        i = 0
         for j in range(len(fruits)):
-            if b1==fruits[j]:
-                b1_count+=1
+            basket[fruits[j]]+=1
 
-            elif b2==fruits[j]:
-                b2_count+=1
-            elif b1==None:
-                b1 = fruits[j]
-                b1_count = 1
-            elif b2==None:
-                b2 = fruits[j]
-                b2_count = 1
-            else:
-                if fruits[j]!=b1 and fruits[j]!=b2:
-                    while(b1_count!=0 and b2_count!=0):
-                        if fruits[i]==b1:
-                            b1_count-=1
-                        elif fruits[i]==b2:
-                            b2_count-=1
-                        i+=1
-                    
-                    if b1_count==0:
-                        b1 = fruits[j]
-                        b1_count = 1
-                    else:
-                        b2 = fruits[j]
-                        b2_count = 1
-                
-            maxi = max(maxi,abs(j-i)+1)
-            
-        return maxi
+            if len(basket)>2:
+                basket[fruits[i]]-=1
+                if basket[fruits[i]]==0:
+                    del basket[fruits[i]]
+                i+=1
+        
+        return j-i+1
