8e731176f7fbc6f75ddb1592139c21639bde258b
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-09-10 20:35:57 +0530

    Time: 143 ms (30.02%), Space: 15.5 MB (65.26%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,3 +1,4 @@
+import collections
 class Solution(object):
     def minWindow(self, s, t):
         """
@@ -5,32 +6,27 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        freq_map = {}
-        for i in t:
-            if i not in freq_map:
-                freq_map[i]=1
-            else:
-                freq_map[i]+=1
-        
-        i = 0
+        freq_map = collections.Counter(t)
+        total_char = len(freq_map)
+        matched = i = 0
         min_i,min_j=0,float('inf')
+
         for j in range(len(s)):
             if s[j] in freq_map:
                 freq_map[s[j]]-=1
-            else:
-                freq_map[s[j]]=-2
-        
-            is_true = True
-            for char in freq_map:
-                if freq_map[char]>0:
-                    is_true = False
-                    break
-            while(is_true and freq_map[s[i]]+1<=0):
-                freq_map[s[i]]+=1
-                i+=1
+                if freq_map[s[j]]==0:
+                    matched+=1
 
-            if min_j-min_i>j-i and is_true:
-                min_i,min_j=i,j
+            while(matched==total_char):
+                if min_j-min_i>j-i:
+                    min_i,min_j=i,j
+                
+                if s[i] in freq_map:
+                    freq_map[s[i]]+=1
+                    if freq_map[s[i]]>0:
+                        matched-=1
+                
+                i+=1
 
         return s[min_i:min_j+1] if min_j!=float('inf') else ""
             
