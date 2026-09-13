9e64ba2a7343d7e1df818f698eb3c0cf7ba68749
RUTVIK MANGROLE <52249937+Rut092@users.noreply.github.com>
2026-09-13 11:04:36 +0530

    Time: 132 ms (26.76%), Space: 14.4 MB (17.18%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.py
@@ -0,0 +1,25 @@
+import heapq
+from collections import Counter
+class Solution(object):
+    def isNStraightHand(self, hand, groupSize):
+        """
+        :type hand: List[int]
+        :type groupSize: int
+        :rtype: bool
+        """
+        freq_map = Counter(hand)
+        h = [i for i in freq_map]
+        heapq.heapify(h)
+
+        while(h):
+            ele = h[0]
+            if ele in freq_map:
+                for i in range(ele,ele+groupSize):
+                    if i not in freq_map:
+                        return False
+                    freq_map[i]-=1
+                    if freq_map[i]==0:
+                        del freq_map[i] 
+            else:
+                heapq.heappop(h)
+        return True
